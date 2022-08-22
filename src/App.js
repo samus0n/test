300ce69b3509b567a2419e9d1b6dc9648805bda4
tatuya <tatuyapiero85@gmail.com>
2022-08-22 13:35:48 +0900

    [Add] daisyUI追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+    <>
    <p>Hello サム</p>
+   <div class="avatar">
+  <div class="w-24 rounded">
+    <img src="./image1.jpg" />
+  </div>
+</div>
+</>
   );
 }
 
