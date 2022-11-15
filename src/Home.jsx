b1f9c876d787cd6dda5077dc3f3decb272a2128c
Shun <syunya.f-0307@i.softbank.jp>
2022-11-15 14:21:24 +0900

    天気追加

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,9 +6,21 @@ const backgroundPicture = new URL("./images/JShine.png",import.meta.url)
 const Home = () => {
   return (
     <section>
-      <div className = "background">
-         <img src = {backgroundPicture} />
-     </div>
+
+      
+
+
+
+
+      
+      
+     <div className='button'>
+      <button class="btn btn-wide" id='a'>八王子駅発</button>
+      <button class="btn btn-wide" id='a'>みなみ野駅発</button><br/>
+      <button class="btn btn-wide" id='a'>学生会館発</button>
+      </div>
+
+
     </section>
   )
 }
