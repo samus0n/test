90b57e7ef9a554fdac4b714d37fa9058eedb9119
samus0n <you.0114.13@gmail.com>
2023-11-16 17:50:48 +0900

    Merge branch 'main' of https://github.com/samus0n/test
    add background

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
       <label tabindex="0" class="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </label>
-      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow  bg-base-100 rounded-box w-52">
         <li><Link to="/" id='navtext'>ホーム</Link></li>
         <li><Link to="/time1" id='navtext'>八王子みなみ野駅</Link></li>
         <li><Link to="/time2"  id='navtext'>八王子駅南口</Link></li>
